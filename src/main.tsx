1151adc073db9522294089cc50a557eecb2a5afc
rafaelclima <rafaelclima.ti@gmail.com>
2024-09-14 19:25:46 -0300

    :sparkles: feat: add react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,26 @@
 import '../src/styles/index.css';
 
-import { StrictMode } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+//import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3,
+      retryDelay: 1000,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  //<StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <App />,
+  </QueryClientProvider>,
+  //</StrictMode>,
 );
