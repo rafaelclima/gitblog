a1656764695b1fe1b40df628362d9a87a6f3a676
rafaelclima <rafaelclima.ti@gmail.com>
2024-09-15 14:06:43 -0300

    :sparkles: feat: Add lógica para paginação

--- src/components/Repos.tsx
+++ src/components/Repos.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useFetchRepoData } from '../services/fetchData';
 interface Repo {
   id: number;
@@ -10,36 +11,65 @@ interface Repo {
   forks_count: number;
 }
 export function Repos() {
-  const { data, isLoading, error } = useFetchRepoData();
+  const [page, setPage] = useState(1);
+  const { data, isPending, isError, error, isFetching, isPlaceholderData } =
+    useFetchRepoData(page);
 
-  if (isLoading) {
+  if (isPending) {
     return <p>Loading...</p>;
   }
-  if (error) {
+  if (isError) {
     return <p>Error: {error.message}</p>;
   }
-  //console.log(data);
+  console.log();
 
   return (
-    <section className=" w-full grid sm:grid-cols-2 gap-4 md:gap-8 justify-items-center mb-[234px]">
-      {data.map((repo: Repo) => (
-        <div
-          key={repo.id}
-          className=" w-full flex flex-col justify-center gap-5 p-8 rounded-[10px] bg-base-post ">
-          <div className=" flex justify-between items-center ">
-            <h2 className=" text-base-title font-bold text-xl leading-160 ">
-              {repo.name}
-            </h2>
-            <span className=" text-base-span font-regular text-sm leading-160 ">
-              {repo.created_at}
-            </span>
+    <main>
+      <section className=" w-full grid sm:grid-cols-2 gap-4 md:gap-8 justify-items-center ">
+        {data?.data.map((repo: Repo) => (
+          <div
+            key={repo.id}
+            className=" w-full flex flex-col justify-center gap-5 p-8 rounded-[10px] bg-base-post ">
+            <div className=" flex justify-between items-center ">
+              <h2 className=" text-base-title font-bold text-xl leading-160 ">
+                {repo.name}
+              </h2>
+              <span className=" text-base-span font-regular text-sm leading-160 ">
+                {repo.created_at}
+              </span>
+            </div>
+            <p className=" text-base-span font-regular text-base leading-160 line-clamp-4 ">
+              {repo.description}
+            </p>
           </div>
-          <p className=" text-base-span font-regular text-base leading-160 line-clamp-4 ">
-            {repo.description}
-          </p>
-        </div>
-      ))}
-      )
-    </section>
+        ))}
+      </section>
+
+      <div className=" w-1/2 ml-auto flex items-center justify-evenly mt-6 mb-16 ">
+        <button
+          type="button"
+          onClick={() => setPage((prev: number) => Math.max(prev - 1, 1))}
+          disabled={page === 1}
+          className={` text-base-text font-semibold focus:outline-none  
+          ${page === 1 ? ' text-base-label cursor-not-allowed' : 'text-base-text hover:text-base-label'}`}>
+          Voltar
+        </button>
+
+        <span className=" text-base-title text-base ">
+          {isFetching ? 'Carregando...' : page}
+        </span>
+
+        <button
+          type="button"
+          onClick={() => {
+            data?.hasMore && setPage((prev: number) => prev + 1);
+          }}
+          disabled={!data?.hasMore}
+          className={` text-base-text font-semibol focus:outline-none  
+          ${isPlaceholderData || !data?.hasMore ? 'text-base-label cursor-not-allowed' : 'text-base-text hover:text-base-label'}`}>
+          Próximo
+        </button>
+      </div>
+    </main>
   );
 }
